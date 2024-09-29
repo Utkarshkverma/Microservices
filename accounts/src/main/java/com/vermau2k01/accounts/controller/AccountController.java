9ef59ae93f2350234f2986fbec55499c4c080341
Utkarsh <vermau2k01@gmail.com>
2024-09-29 09:53:27 +0530

    Created APIs for all three microservices

---
+++ accounts/src/main/java/com/vermau2k01/accounts/controller/AccountController.java
@@ -0,0 +1,243 @@
+package com.vermau2k01.accounts.controller;
+
+import com.vermau2k01.accounts.constants.AppConstants;
+import com.vermau2k01.accounts.dto.CustomerDto;
+import com.vermau2k01.accounts.dto.ErrorResponseDto;
+import com.vermau2k01.accounts.dto.ResponseDto;
+import com.vermau2k01.accounts.service.IAccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@RequiredArgsConstructor
+@Validated
+@Tag(
+        name = "CURD REST API for QBank",
+        description = "CURD REST API for QBank to create, update, fetch and delete account details "
+)
+public class AccountController {
+
+    //@Value("${build.version}")
+    private String buildVersion = "2.2";
+
+    private final IAccountService accountService;
+    private final Environment environment;
+
+
+    @Operation(
+            summary = "Create Account REST API",
+            description = "REST API to create new Customer &  Account inside QBank"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status CREATED"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
+        accountService.createAccount(customerDto);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AppConstants.STATUS_201,
+                        AppConstants.MESSAGE_201));
+    }
+
+
+    @Operation(
+            summary = "Fetch Account Details REST API",
+            description = "REST API to fetch Customer &  Account details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/fetch")
+    public ResponseEntity<CustomerDto>
+    fetchAccount(@RequestParam
+                 @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits") String mobileNumber) {
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .body(accountService.fetchAccount(mobileNumber));
+    }
+
+
+    @Operation(
+            summary = "Update Account Details REST API",
+            description = "REST API to update Customer &  Account details based on a account number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
+        boolean isUpdated = accountService.updateAccount(customerDto);
+        if(isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(AppConstants.STATUS_200, AppConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AppConstants.STATUS_417, AppConstants.MESSAGE_417_UPDATE));
+        }
+    }
+
+    @Operation(
+            summary = "Delete Account & Customer Details REST API",
+            description = "REST API to delete Customer &  Account details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDto> deleteAccountDetails(@RequestParam
+                                                            @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+                                                            String mobileNumber) {
+        boolean isDeleted = accountService.deleteAccount(mobileNumber);
+        if(isDeleted) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(AppConstants.STATUS_200, AppConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AppConstants.STATUS_417, AppConstants.MESSAGE_417_DELETE));
+        }
+    }
+
+//    @Operation(
+//            summary = "Get Build version",
+//            description = "Get Build versions details of the accounts microservice"
+//    )
+//    @ApiResponses({
+//            @ApiResponse(
+//                    responseCode = "200",
+//                    description = "HTTP Status OK"
+//            ),
+//            @ApiResponse(
+//                    responseCode = "500",
+//                    description = "HTTP Status Internal Server Error",
+//                    content = @Content(
+//                            schema = @Schema(implementation = ErrorResponseDto.class)
+//                    )
+//            )
+//    }
+//    )
+//    @GetMapping("build-info")
+//    public ResponseEntity<String> buildInfo() {
+//        return ResponseEntity.ok().body(buildVersion);
+//    }
+//
+//    @Operation(
+//            summary = "Get Java version",
+//            description = "Get Java versions details that is installed into accounts microservice"
+//    )
+//    @ApiResponses({
+//            @ApiResponse(
+//                    responseCode = "200",
+//                    description = "HTTP Status OK"
+//            ),
+//            @ApiResponse(
+//                    responseCode = "500",
+//                    description = "HTTP Status Internal Server Error",
+//                    content = @Content(
+//                            schema = @Schema(implementation = ErrorResponseDto.class)
+//                    )
+//            )
+//    }
+//    )
+//    @GetMapping("/java-version")
+//    public ResponseEntity<String> javaVersion() {
+//        return ResponseEntity.ok().body(environment.getProperty("JAVA_HOME"));
+//    }
+//
+//    @Operation(
+//            summary = "Get Contact Info",
+//            description = "Contact Info details that can be reached out in case of any issues"
+//    )
+//    @ApiResponses({
+//            @ApiResponse(
+//                    responseCode = "200",
+//                    description = "HTTP Status OK"
+//            ),
+//            @ApiResponse(
+//                    responseCode = "500",
+//                    description = "HTTP Status Internal Server Error",
+//                    content = @Content(
+//                            schema = @Schema(implementation = ErrorResponseDto.class)
+//                    )
+//            )
+//    }
+//    )
+//    @GetMapping("/contact-info")
+//    public ResponseEntity<AccountContactInfoDto> accountContactInfo() {
+//        return ResponseEntity.ok().body(accountContactInfoDto);
+//    }
+
+
+
+
+}
---
+++ cards/src/main/java/com/vermau2k01/cards/entity/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.vermau2k01.cards.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+
+@MappedSuperclass
+@Getter
+@Setter
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @Column(updatable = false)
+    private LocalDate createdAt;
+    @Column(updatable = false)
+    private String createdBy;
+    @Column(insertable = false)
+    private LocalDate updatedAt;
+    @Column(insertable = false)
+    private String updatedBy;
+}
